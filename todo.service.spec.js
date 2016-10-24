cb4693cfb0cbfbca34370e8f22c9ebe127c5fb50
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2016-10-24 22:30:36 +0200

    Initial commit

--- todo.service.spec.js
+++ todo.service.spec.js
@@ -17,16 +17,9 @@ describe('todoService', function() {
             todoService.add(todo);
 
             expect(todoService.todos).length.to.be.above(1);
-
         });
     });
-
-    describe('check', function() {
-        it('should set')
-    });
-
 });
-
 
 function setup() {
     angular.mock.module('todo', function($provide) {
@@ -34,5 +27,44 @@ function setup() {
     });
     angular.mock.inject(function(TodoService) {
         todoService = TodoService;
-    });
+    })
 }
+
+// beforeEach(function() {
+//     setup();
+// });
+
+// describe('todoService', function() {
+//     it('should exist', function() {
+//         expect(todoService).to.exist;
+//     });
+//
+//     describe('add', function() {
+//         it('should add a todo to the todos array', function() {
+//             var todo = {
+//                 title: 'New todo',
+//                 state: 'open'
+//             };
+//
+//             todoService.add(todo);
+//
+//             expect(todoService.todos).length.to.be.above(1);
+//
+//         });
+//     });
+//
+//     describe('check', function() {
+//         it('should set')
+//     });
+//
+// });
+
+
+// function setup() {
+//     angular.mock.module('todo', function($provide) {
+//         $provide.value('TODO_STATUS', {});
+//     });
+//     angular.mock.inject(function(TodoService) {
+//         todoService = TodoService;
+//     });
+// }
