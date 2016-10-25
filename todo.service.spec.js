7376c3057851a8c4d1712079a63cf68c93dae42e
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2016-10-25 16:10:26 +0200

    - use strict - global var todoService - Add test for calling add without parameters - constant provide.value > provide.constant

--- todo.service.spec.js
+++ todo.service.spec.js
@@ -1,3 +1,7 @@
+'use strict';
+
+var todoService;
+
 beforeEach(function() {
     setup();
 });
@@ -18,53 +22,21 @@ describe('todoService', function() {
 
             expect(todoService.todos).length.to.be.above(1);
         });
+
+        it('should return undefined if no todo is supplied', function() {
+            var todos = todoService.add();
+
+            expect(todos).to.be.undefined;
+        })
     });
 });
 
+
 function setup() {
     angular.mock.module('todo', function($provide) {
-        $provide.value('TODO_STATUS', {});
+        $provide.constant('TODO_STATUS', {test:"test"});
     });
     angular.mock.inject(function(TodoService) {
         todoService = TodoService;
     })
 }
-
-// beforeEach(function() {
-//     setup();
-// });
-
-// describe('todoService', function() {
-//     it('should exist', function() {
-//         expect(todoService).to.exist;
-//     });
-//
-//     describe('add', function() {
-//         it('should add a todo to the todos array', function() {
-//             var todo = {
-//                 title: 'New todo',
-//                 state: 'open'
-//             };
-//
-//             todoService.add(todo);
-//
-//             expect(todoService.todos).length.to.be.above(1);
-//
-//         });
-//     });
-//
-//     describe('check', function() {
-//         it('should set')
-//     });
-//
-// });
-
-
-// function setup() {
-//     angular.mock.module('todo', function($provide) {
-//         $provide.value('TODO_STATUS', {});
-//     });
-//     angular.mock.inject(function(TodoService) {
-//         todoService = TodoService;
-//     });
-// }
